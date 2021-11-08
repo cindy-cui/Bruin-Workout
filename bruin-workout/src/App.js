e79880083b969a4b9a31d869299071941b735555
briannparkk <63471459+briannparkk@users.noreply.github.com>
2021-11-08 15:19:25 -0800

    Implemented structure of the login page. Added pictures of The Rock, Email and Password input fields, and buttons to login/signup.

--- bruin-workout/src/App.js
+++ bruin-workout/src/App.js
@@ -1,22 +1,24 @@
 import './styles/App.css';
+import LoginPages from './components/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <LoginPages/>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
--- bruin-workout/src/components/Login.js
+++ bruin-workout/src/components/Login.js
@@ -1,7 +1,32 @@
-import '../styles/Login.css'
+import '../styles/Login.css';
+import rockSerious from '../assets/rock-serious.jpeg';
+import rockSmiling from '../assets/rock-smiling.jpeg';
+import rockFlexing from '../assets/rock-flexing.png';
 
 export default function LoginPages() {
-    render (
-        <h1 class="login-header">Face Off</h1>
+    return (
+        <div class="login-page">
+            <h1>Face Off</h1>
+            <br></br>
+            <h4>Welcome</h4>
+            <form>
+                <img src={rockSerious} alt="" class="rock-serious"/>
+                <img src={rockFlexing} alt="" class="rock-flexing1"/>
+                <img src={rockFlexing} alt="" class="rock-flexing2"/>
+                <img src={rockSmiling} alt="" class="rock-smiling"/>
+                <div class="login-field">
+                    <label for="userEmail"></label>
+                    <input placeholder="Email Address"></input>
+                </div>
+                <div class="login-field">
+                    <label for="userPassword"></label>
+                    <input placeholder="Password"></input>
+                </div>
+                <div class="login-buttons">
+                    <input type="submit" value="LOG IN" class="login-button"></input><br></br>
+                    <input type="submit" value="SIGN UP" class="login-button"></input>
+                </div>
+            </form>
+        </div>
     );
 }
