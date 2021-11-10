2c7f0212385b8c98ff36582c8fe14cd866f070bf
Dennis Li <62808150+lidennis610@users.noreply.github.com>
2021-11-10 15:24:44 -0800

    Update Information.js
    created a user class with instance variables

---
+++ bruin-workout/src/components/Information.js
@@ -0,0 +1,10 @@
+class User{
+    constructor(){
+        this.name = myName;
+        this.age = age;
+        this.ethnicity = ethnicity;
+        this.height = height;
+        this.gender = gender;
+        this.favWorkout = favWorkout;
+    }
+}
