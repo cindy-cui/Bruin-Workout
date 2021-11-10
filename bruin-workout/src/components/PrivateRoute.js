ebee516ce55d674b0cb51cc9c833bcc50807b6f8
sdaniel2 <93291598+sdaniel2@users.noreply.github.com>
2021-11-10 12:04:36 -0800

    Initialize Firebase
    Initialize Firebase
    
    Use initializeApp with a config conveniently provided from the Firebase console.

--- bruin-workout/src/components/PrivateRoute.js
+++ bruin-workout/src/components/PrivateRoute.js
@@ -1,5 +1,28 @@
 import React from 'react';
 import { Navigate} from 'react-router-dom';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAoyhG9AA-GVRoCfTMoJXDQiSXCdLaKJPM",
+  authDomain: "bruin-workout-6a1e3.firebaseapp.com",
+  projectId: "bruin-workout-6a1e3",
+  storageBucket: "bruin-workout-6a1e3.appspot.com",
+  messagingSenderId: "1024573966853",
+  appId: "1:1024573966853:web:2ce8d5626e4204d3e3aa6c",
+  measurementId: "G-ZJGBC4EGC7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+const app = initializeApp(firebaseConfig);
 
 const PrivateRoute = () => {
     const auth = null; // determine if authorized
