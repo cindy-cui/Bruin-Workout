1e5a073ec916c088724f7154d6769ff06c9edf63
sdaniel2 <93291598+sdaniel2@users.noreply.github.com>
2021-11-13 16:58:46 -0800

    Update Auth.js
    Commented out the unnecessary function calls in Auth.js

--- bruin-workout/src/components/Auth.js
+++ bruin-workout/src/components/Auth.js
@@ -3,22 +3,11 @@ import app from "./FirebaseConfig";
 import { getAuth, createUserWithEmailAndPassword,signInWithEmailAndPassword ,onAuthStateChanged ,signOut } from "firebase/auth";
 
 //keep track of state changes
-<<<<<<< Updated upstream
 /*onAuthStateChanged(auth, (user) => {
   if (user) {
     // User is signed in, see docs for a list of available properties
     // https://firebase.google.com/docs/reference/js/firebase.User
     const uid = user.uid;
-=======
-onAuthStateChanged(auth, (user) => {
-  if (user) {
-    // User is signed in, see docs for a list of available properties
-    // https://firebase.google.com/docs/reference/js/firebase.User
-
-    //initialize properties for each user
-    const uid = user.uid; 
-    const email = user.email; 
->>>>>>> Stashed changes
     // ...
   } else {
     // User is signed out
@@ -32,10 +21,6 @@ signOut(auth).then(() => {
 }).catch((error) => {
   // An error happened.
 });
-<<<<<<< Updated upstream
 */
-=======
-
->>>>>>> Stashed changes
 const auth= getAuth(app);
 export default auth;
