43afa9828010909d1d283e2e7ca4789b618ff985
sdaniel2 <93291598+sdaniel2@users.noreply.github.com>
2021-11-19 15:55:31 -0800

    Home.js Login check
    Check if there is a user logged in; if not send to login page
    
    This is done for security reasons, and also because when the user refreshes the browser sometimes the auth instance is set to null (which means there is no user logged in).

--- bruin-workout/src/pages/Home.js
+++ bruin-workout/src/pages/Home.js
@@ -1,7 +1,11 @@
 import NavigationBar from '../components/NavigationBar';
 import auth from '../components/Auth';
+import ProfileSearch from '../components/ProfileSearch';
+import {useNavigate} from "react-router-dom";
+import { useEffect } from 'react';
 
 export default function Home(props){ //props.id stores the current user's id
+    let navigate=useNavigate();
     function display(){
         let user=auth.currentUser;
         if(user===null)
@@ -9,13 +13,22 @@ export default function Home(props){ //props.id stores the current user's id
         else{
             console.log(user.uid);
             return user.displayName;
-        } 
-        
+        }
     }
+    //if user is not logged in, send user back to login page.
+    useEffect(
+        ()=>{
+            if(auth.currentUser===null){
+                navigate("/login");
+            }
+        }
+    );
+
     return(<div>
         <NavigationBar page="Home"/>
         <h1>
             Welcome, {display()}
         </h1>
+        <ProfileSearch />
     </div>);
 }
