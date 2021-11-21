0d6bae91f3a24cdc05b9bf568d2acfc8c68417db
benson <bensonhar50@gmail.com>
2021-11-20 21:56:15 -0800

    Implemented a general layout for the main page
    Implemented the layout for the main page where the workouts and schedule are going to be. Used Grid from material-ui to do so and created a style file
    that helps make the layout look right. Uses a temporary Item component to show where components are going to be laid out on the screen when inserted.
    Header also has a profile button that has not been linked to the routes yet.

--- bruin-workout/src/components/Layout.js
+++ bruin-workout/src/components/Layout.js
@@ -1,65 +1,92 @@
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import { makeStyles } from '@material-ui/styles';
-import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardHeader from '@mui/material/CardHeader';
-import CssBaseline from '@mui/material/CssBaseline';
 import Grid from '@mui/material/Grid';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Link from '@mui/material/Link';
-import GlobalStyles from '@mui/material/GlobalStyles';
 import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import styles from '../styles/styles'
 
-const useStyles = makeStyles((theme) => {
-    return {
-        toolbar: theme.mixins.toolbar,        
-    }
-})
+import { styled } from '@mui/material/styles';
 
-export default function Layout() {
-  return (
-    <div>
-        <AppBar
-            position="static"
-            elevation={0}
-            sx={{ borderBottom: (theme) => `1px solid ${theme.palette.divider}` }}
-        >
-            <Toolbar >
-                <nav>
-                    <Link
-                        variant="button">
-                        Profile
-                    </Link>
-                </nav>
-            </Toolbar>
-        </AppBar>
-        <Container maxWidth="md" maxHeight="md">
-            <Grid container direction="row">
-                <Grid container item direction="column">
-                    <Grid item xs={4}>
-                        {/* Filter feature */}
-                    </Grid>
-                    <Grid item xs={4}>
-                        {/* Planner */}
-                    </Grid>
+const useStyles = makeStyles(styles)
+
+// Item is a placeholder for the actual components. Will be useful as a visual aid to see how 
+// the main page is laid out. 
+const Item = styled(Paper)(({ theme }) => ({
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  }));
+
+function WorkoutListAndFilter() {
+    return(
+        <React.Fragment>
+            <Grid item>
+                {/* Filter feature */}
+                <Item>Item</Item>
+            </Grid>
+            <Grid item>
+                {/* Planner */}
+                <Item>Item</Item>
+            </Grid>
+        </React.Fragment>
+    );
+}
+
+function ScheduleAndSubWindows() {
+    return(
+        <React.Fragment>
+            <Grid item>
+                <Item>Item</Item>                            
+            </Grid>
+            <Grid container item direction="row" xs={4} sm={4}  md={4} spacing={10}>
+                <Grid item>
+                    <Item>Item</Item>
                 </Grid>
-                <Grid container item direction="column">
-                    <Grid item xs={6}>
-                    </Grid>
-                    <Grid container item direction="row">
-                        <Grid item xs={2}>
-                        </Grid>
-                        <Grid item xs={2}>
-                        </Grid>
-                    </Grid>
+                <Grid item>
+                    <Item>Item</Item>                            
                 </Grid>
             </Grid>
-        </Container>
-    </div>
+        </React.Fragment>
+    );
+}
+
+export default function Layout() {
+    const classes = useStyles()
+    return (
+        <React.Fragment>
+            <AppBar
+                position="static"
+                elevation={0}>
+                <Toolbar >
+                    <Typography variant="title" color="inherit" sx={{ flexGrow: 1 }}>
+                        Bruin Workout
+                    </Typography>
+                    <nav>
+                        <Button variant="text" color="inherit">Profile</Button>
+                    </nav>
+                </Toolbar>
+            </AppBar>
+            <Box t={10} l={10} className={classes.container}>
+                <Grid container columnSpacing={10} columns={11}>
+                    <Grid container item direction="column" xs={4} sm={4} md={4} lg={4} rowSpacing={3}>
+                        <WorkoutListAndFilter />
+                    </Grid>
+                    <Grid container item direction="column" xs={7} sm={7} md={7} rowSpacing={3}>
+                        <ScheduleAndSubWindows/>
+                    </Grid>
+                </Grid>
+            </Box>
+        </React.Fragment>
   );
 }
---
+++ bruin-workout/src/styles/styles.js
@@ -0,0 +1,13 @@
+export default {
+    page: {
+        background: '#1F2833',
+        // width: '100vw',
+        height: '100vh'
+    },
+    container: {
+        padding: '5vw'
+    },
+    profileButton: {
+        color: '#FFFFFF '
+    }
+}
