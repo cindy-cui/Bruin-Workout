c8ff8b0055d348f5bcbcce03327a2f3684168d30
benson <bensonhar50@gmail.com>
2021-11-28 21:22:45 -0800

    Implemented the planner for displaying the schedule
    Implemented a PlannerItem component to populate the Planner component. Using a sample workout since the Workout.js file doesn't seem to be compile.

--- bruin-workout/src/components/home/Planner.js
+++ bruin-workout/src/components/home/Planner.js
@@ -1,10 +1,38 @@
 //the planner showing the workouts
 import React from "react";
+import Grid from '@mui/material/Grid';
+import PlannerItem, { plannerInfo } from './PlannerItem'
+// import { Workouts } from '../Workout'
 
+let Workouts = { name: "bench press" }
+let sample = plannerInfo("Monday", Workouts)
 export default function Planner() {
     return (
-        <div className="workout-planner">
-            Planner
-        </div>
+        <Grid container direction="row">
+            <Grid item>
+                <PlannerItem info={sample} />
+            </Grid>
+            <Grid item>
+                <PlannerItem info={sample} />
+            </Grid>
+            <Grid item>
+                <PlannerItem info={sample} />
+            </Grid>
+            <Grid item>
+                <PlannerItem info={sample} />
+            </Grid>
+            <Grid item>
+                <PlannerItem info={sample} />
+            </Grid>
+            <Grid item>
+                <PlannerItem info={sample} />
+            </Grid>
+            <Grid item>
+                <PlannerItem info={sample} />
+            </Grid>
+        </Grid>
+        // <div className="workout-planner">
+        //     Planner
+        // </div>
     )
 }
---
+++ bruin-workout/src/components/home/PlannerItem.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { Card, CardHeader, CardMedia, CardContent } from '@mui/material';
+import { Container, Typography } from "@mui/material";
+
+export function plannerInfo(day, workout) {
+    return {
+        day,
+        workout
+    }
+}
+
+export default function PlannerItem({ info }) {
+    return (
+        // <Container>
+            <Card variant="outlined" className="planner-item">
+                {/* <Typography variant="h7">
+                    {info.day}
+                </Typography> */}
+                <CardHeader title={ info.day } />
+                {/* <CardMedia
+                    component="img"
+                    height="100"
+                    image={ info.workout.img }
+                    alt={ info.workout.alt }
+                /> */}
+                <CardContent>
+                    <Typography>
+                        { info.workout.name }
+                    </Typography>
+                </CardContent>
+            </Card>
+        // </Container>
+    )
+}
