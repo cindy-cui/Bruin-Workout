c7f856991eb21792adf8fcc7a1702fc2cb4215df
briannparkk <63471459+briannparkk@users.noreply.github.com>
2021-11-30 12:14:56 -0800

    started working on the filter feature

--- bruin-workout/src/pages/Home.js
+++ bruin-workout/src/pages/Home.js
@@ -43,6 +43,7 @@ function WorkoutListAndFilter(props) {
                 <List handleClick={ props.handleClick }/>
             </Grid>
             <Grid item>
+                <Filters />
                 {/*<Filters />*/}
             </Grid>
         </React.Fragment>
